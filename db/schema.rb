6180a09a80fb6ad4d7cf8adabcc99ab1c0b509b5
Huwaida1990 <huwaida_khaild@yahoo.com>
2022-07-17 12:41:54 -0600

    ready spec

--- db/schema.rb
+++ db/schema.rb
@@ -54,9 +54,9 @@ ActiveRecord::Schema.define(version: 2022_07_07_151618) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
