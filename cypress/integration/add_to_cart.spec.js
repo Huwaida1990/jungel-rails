a26fde430301545b71bfbc7ab9ad10f8eba610f8
Huwaida1990 <huwaida_khaild@yahoo.com>
2022-07-20 16:52:14 -0600

    feature/testing-add-to-cart

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,10 @@
+describe("Testing Home page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("allows users can click the 'Add to Cart' button for a product on the home page and in the cart increases by one", () => {
+    cy.get(".btn").contains("Add").click({ force: true });
+    cy.get(".nav-link").should("contain", 1);
+  });
+});
