d348ba92b2c10825a0b3674b364d42a35c93bdf2
Huwaida1990 <huwaida_khaild@yahoo.com>
2022-07-19 21:24:09 -0600

    testing-product-page

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,14 @@
+describe("Testing Home page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("allows users to navigate from home page to product detail page by clicking on a product", () => {
+    cy.get(".products article")
+      .eq(1)
+      .click()
+      .then(() => {
+        return ".product-detail";
+      });
+  });
+});
